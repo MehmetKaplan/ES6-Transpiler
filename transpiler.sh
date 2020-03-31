d2357427085d24dd701954ff5be41bf1dd072f87
Mehmet Kaplan <mehmet.kaplan@gmail.com>
2020-04-01 00:45:24 +0630

    .

--- transpiler.sh
+++ transpiler.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
   
 if [ $# -ne 1 ]; then
-        echo -e "\n\n\n\n\t\t\t\033[37mUsage:\n\n\n\t\t\033[34mtranspiler.sh \033[36mPATH-TO-DIRECTORY-OR-SINGLE-SOURCE-IN-ES6\033[31m\n\n\n"
+        echo -e "\n\n\n\n\t\t\t\E[37mUsage:\n\n\n\t\t\E[34mtranspiler.sh \E[36mPATH-TO-DIRECTORY-OR-SINGLE-SOURCE-IN-ES6\E[31m\n\n\n"
 else
 	echo -e "\033[33mCleaning: \033[31m./src/\033[37m"
 	rm -r ./src
